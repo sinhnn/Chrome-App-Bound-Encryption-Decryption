c335833e1a3dab3f74fafd134faa15c17396c3fa
sinhnn <sinhnn.92@gmail.com>
2026-08-25 17:56:58 +0700

    draft  commit

--- src/injector/injector_api.hpp
+++ src/injector/injector_api.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
 #include "../payload/messages.hpp"
+#include "pipe_server.hpp"
 
 namespace Injector {
 
-Payload::Message* ProcessBrowser(void* request_message);
+class PipeServer_EXT : public PipeServer
+{
+    public:
+        explicit PipeServer_EXT(const std::wstring& browserType) : PipeServer(browserType) {}
+        int Send(Payload::Request& request, bool verbose);
+        void SendMessage(const Payload::request_msg& request, bool verbose);
+        void WaitResponse(const Payload::request_msg& request, Payload::response_msg& response, bool verbose, uint32_t timeout);
+        void WaitAck(const Payload::request_msg& request, bool verbose, uint32_t timeout);
+        void SendAck(const Payload::request_msg& request, bool verbose, uint32_t timeout);
+};
 
-}
+
+int Process(Payload::Request& request);
+
+} // namespace Injector
